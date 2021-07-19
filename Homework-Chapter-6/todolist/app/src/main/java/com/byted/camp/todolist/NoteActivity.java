fd01ae74607310172f7aa32847da0ab51ab5dd64
C-ld <zjuludi@126.com>
2021-07-19 23:19:19 +0800

    day6

---
+++ Homework-Chapter-6/todolist/app/src/main/java/com/byted/camp/todolist/NoteActivity.java
@@ -0,0 +1,163 @@
+package com.byted.camp.todolist;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatRadioButton;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.byted.camp.todolist.beans.Priority;
+import com.byted.camp.todolist.beans.State;
+import com.byted.camp.todolist.db.TodoContract.TodoNote;
+import com.byted.camp.todolist.db.TodoDbHelper;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class NoteActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button addBtn;
+    private RadioGroup radioGroup;
+    private AppCompatRadioButton lowRadio;
+
+
+    private TodoDbHelper dbHelper;
+    private SQLiteDatabase database;
+
+    private String path;
+    File logFile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+        setTitle(R.string.take_a_note);
+
+        dbHelper = new TodoDbHelper(this);
+        database = dbHelper.getWritableDatabase();
+
+        editText = findViewById(R.id.edit_text);
+        editText.setFocusable(true);
+        editText.requestFocus();
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (inputManager != null) {
+            inputManager.showSoftInput(editText, 0);
+        }
+        radioGroup = findViewById(R.id.radio_group);
+        lowRadio = findViewById(R.id.btn_low);
+        lowRadio.setChecked(true);
+
+        addBtn = findViewById(R.id.btn_add);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CharSequence content = editText.getText();
+                if (TextUtils.isEmpty(content)) {
+                    Toast.makeText(NoteActivity.this,
+                            "No content to add", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                boolean succeed = saveNote2Database(content.toString().trim(),
+                        getSelectedPriority());
+                if (succeed) {
+                    editText.setText("");
+                    Toast.makeText(NoteActivity.this,
+                            "Note added", Toast.LENGTH_SHORT).show();
+                    setResult(Activity.RESULT_OK);
+                } else {
+                    Toast.makeText(NoteActivity.this,
+                            "Error", Toast.LENGTH_SHORT).show();
+                }
+                finish();
+            }
+        });
+        getFilePath();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        SharedPreferences settings = this.getSharedPreferences(
+                "PREFS_CONF",
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor edtor=settings.edit();
+        String str=settings.getString("key", "");
+        editText.setText(str);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SharedPreferences settings = this.getSharedPreferences("PREFS_CONF", Context.MODE_PRIVATE);
+        SharedPreferences.Editor edtor=settings.edit().putString("key", editText.getText().toString()); edtor.commit();
+    }
+
+    private void getFilePath() {
+        try {
+            File fileStorageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+            logFile = new File(fileStorageDir, "tmpDiary.txt");
+            if(!logFile.exists()){
+                logFile.createNewFile();
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+//        Log.d(TAG, "getOutputMediaPath: "+mediaFile.toString());
+        return;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        database.close();
+        database = null;
+        dbHelper.close();
+        dbHelper = null;
+    }
+
+    private boolean saveNote2Database(String content, Priority priority) {
+        // TODO: 2021/7/19 8. 这里插入数据库
+        if (database == null || TextUtils.isEmpty(content)) {
+            return false;
+        }
+        ContentValues values = new ContentValues();
+        values.put(TodoNote.COLUMN_CONTENT, content);
+        values.put(TodoNote.COLUMN_STATE, State.TODO.intValue);
+        values.put(TodoNote.COLUMN_DATE, System.currentTimeMillis());
+        values.put(TodoNote.COLUMN_PRIORITY, priority.intValue);
+        long rowId = database.insert(TodoNote.TABLE_NAME, null, values);
+        return rowId != -1;
+    }
+
+    private Priority getSelectedPriority() {
+        switch (radioGroup.getCheckedRadioButtonId()) {
+            case R.id.btn_high:
+                return Priority.High;
+            case R.id.btn_medium:
+                return Priority.Medium;
+            default:
+                return Priority.Low;
+        }
+    }
+}
